58fa9dd88480ce35d4183755551d8f60fc83ce70
Luca Russo <zwolfrost@gmail.com>
2026-09-11 13:20:00 +0200

    auto minwidth

--- strindex/gui.py
+++ strindex/gui.py
@@ -287,7 +287,6 @@ class MainStrindexGUI(BaseStrindexGUI):
 			if hasattr(self, "tab_widget") else self.sizeHint().height()
 		)
 
-		self.setMinimumWidth(450)
 		self.setMaximumWidth(1280)
 		self.setFixedHeight(height_hint)
 
